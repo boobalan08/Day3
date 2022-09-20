47158a243d71d1b0bb90b78725f137d6b28a9d0f
Boo <111474590+boobalan08@users.noreply.github.com>
2022-09-20 14:01:24 +0530

    Add files via upload

---
+++ Day3/JSON resume/script.js
@@ -0,0 +1,45 @@
+let myResume={
+    "basics": {
+      "name": "BOOBALAN S",
+      "email": "boobalans42@gamil.com",
+      "phone": 7373908825,
+      "degree": "B.E MECHANICAL ENGINEERING",
+      "location": {
+        "address": "35/57k Bharathiyar street",
+        "postalCode": 636102,
+        "city": "Attur",
+        "district":"Salem",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          "website": "https://github.com/boobalan08/mywebsite.git",
+          "github":"https://github.com/boobalan08"
+        }
+      ]
+    },
+    "education": [
+      {
+        "institution": "VSA Group of Institutions",
+        "department": "Mechanical",
+        "batch start year": 2016,
+        "batch end year": 2020,
+        "cgpa": 6.97,
+      }
+    ],
+    "skills": [
+      {
+        "name": "python,javascript,html,css",
+        "level": "beginner"
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,English",
+      }
+    ]
+   
+  }
+  console.log(myResume);
+  
